87e047c776b8c861ec77268c9e36f5c453a80ccc
Ricsi <thereallemonmail@gmail.com>
2024-02-29 19:26:23 +0100

    comments added to settings.blade.php

--- resources/views/includes/settings.blade.php
+++ resources/views/includes/settings.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 @section('content')
+  
+{{--TODO: set these into a card + frontend, and possibly make a change option next to them --}}
 <h2>Profile Details</h2>
 <p><strong>Name: </strong> {{ $user->username }}</p>
 <p><strong>Email: </strong> {{ $user->email }}</p>
-
+{{-- show currency, list out all categories --}}
 
 @endsection
