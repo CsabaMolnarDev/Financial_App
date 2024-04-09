08795b35032354eef76ec94a3f90298377808901
CsabaMolnar <csabusmolnar@gmail.com>
2024-04-09 15:10:56 +0200

    Changed homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,10 @@
     <div id="homeContainer" class="container d-flex align-items-center justify-content-center" style="height: 80vh;">
         <div class="row">
             <h3 class="text-warning">Some content will come here later</h3>
-            <h3 class="text-warning">Total download count maybe with graph etc.</h3>
+            @foreach ($userCount as $item)
+                <h3 class="text-info">Total user count: {{ $item->id }}</h3>
+            @endforeach
             <h3 class="text-warning">Statistics...</h3>
-            <h3 class="text-warning">User manual(how to use the product)....</h3>
         </div>
     </div>
     <div class="container d-flex align-items-center justify-content-center">
