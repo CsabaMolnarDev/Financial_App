e8a7bbb575d9097f5488a41e924b179499564d08
CsabaMolnar <csabusmolnar@gmail.com>
2024-04-16 13:02:47 +0200

    Worked on adv statictics

--- resources/views/includes/advancedStatistics.blade.php
+++ resources/views/includes/advancedStatistics.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.app')
 @section('content')
-
+    <div class="container">
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <div class="card bg-dark text-light">
+                    <div class="card-body">
+                        {{-- Work here --}}
+                    </div>
+                </div>
+            </div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+    <script>
+        /* Bg img */ /* Replace "documentation" with the picture we want */
+        document.body.style.backgroundImage = "url('../storage/pictures/documentation.jpg')";
+    </script>
 @endsection
